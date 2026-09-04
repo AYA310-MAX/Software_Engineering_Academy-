99f1cb2e05a14b7279b5db9e1af34e95ea8cc533
Ayanda Dlamini <aya.softwaredev07@gmail.com>
2026-09-04 22:59:34 +0200

    Complete Day 5 function practice

---
+++ Python/Day05/Day5.2.py
@@ -0,0 +1,11 @@
+def calculate_total(price, quantity):
+    return price * quantity
+
+total = calculate_total(50, 3)
+print(total)
+
+total = calculate_total(100, 5)
+print(total)
+
+total = calculate_total(25, 10)
+print(total)
